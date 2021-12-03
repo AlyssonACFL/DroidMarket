88bf2399a2bb2aecb05679710c61cd77d963da66
AlyssonACFL <63248498+AlyssonACFL@users.noreply.github.com>
2021-12-02 22:32:30 -0300

    saving changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { View } from "react-native";
+import Add from "./produto/input/Add";
+import Produto from "../models/Produto";
+
+export let database : Produto[] = [];
+
 
 const Home = () =>{
     return (
         <View>
-            <title>Home</title>
+            <Add></Add>
         </View>
     )
 }
--- src/components/produto/input/Add.tsx
+++ src/components/produto/input/Add.tsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { Button, View } from "react-native";
 import Produto from "../../../models/Produto";
 import Form from "./Form";
+import {database} from "../../Home"
 
+function salvar ( produto :Produto ){
+    console.log("Novo Produto:", produto)
+    database.push(produto)
+}
 
 const Add = () => {
-    const produto: Produto = {
-        nomeProduto: " "
-    }
+    const [produto, setProduto] = useState<Partial<Produto>>({}); 
     return (
         <View>
-            <Form produto = {produto}/>
+            <Form produto = {produto as Produto} setProduto={setProduto}/>
+            <Button title='Salvar'
+                onPress={() => {
+                salvar(produto as Produto)
+            }}/>
         </View>
     )
 }
