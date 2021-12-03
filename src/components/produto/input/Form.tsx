e8733873179bf5f7c785e47243edd96bbb3db747
AlyssonACFL <63248498+AlyssonACFL@users.noreply.github.com>
2021-12-02 22:32:52 -0300

    saving changes

--- src/components/produto/input/Form.tsx
+++ src/components/produto/input/Form.tsx
@@ -1,29 +1,91 @@
 import React, { useState } from "react";
-import { TextInput, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import Produto from "../../../models/Produto";
-
+import DateField from 'react-native-datefield'
 interface Input {
     produto: Produto
+    setProduto : any
 }
 
-const Form = ( {produto} : Input  ) => {
-    let setNomeProduto;
-    [produto.nomeProduto, setNomeProduto] = useState("");
+const Form = ( {produto, setProduto} : Input  ) => {
 
     return (
         <View>
-            <TextInput placeholder="Código do Produto" />
+            <TextInput placeholder="Código do Produto" 
+                keyboardType='numeric'
+                value={produto.codigo?.toString()}
+                onChangeText = {codigo => setProduto({
+                    ...produto,
+                    codigo: codigo
+                })}
+                />
             <TextInput placeholder="Nome do Produto" 
                 value={produto.nomeProduto}
-                onChangeText={ setNomeProduto }
+                onChangeText = {nome => setProduto({
+                    ...produto,
+                    nomeProduto: nome
+                })}
                 />
-            <TextInput placeholder="Seção do Produto" />
-            <TextInput placeholder="Valor do Produto" />
-            <TextInput placeholder="Quantidade do Produto" />
-            <TextInput placeholder="Unidade do Produto" />
-            <TextInput placeholder="Marca do Produto" />
-            <TextInput placeholder="Validade do Produto" />
-            <TextInput placeholder="Fabricação do Produto" />
+            <TextInput placeholder="Seção do Produto" 
+            value={produto.secao}
+            onChangeText = {secao => setProduto({
+                ...produto,
+                secao: secao
+            })}
+            />
+            <TextInput placeholder="Valor do Produto" 
+                keyboardType='numeric'
+                value={produto.valor?.toString()}
+                onChangeText = {valor => setProduto({
+                    ...produto,
+                    valor: valor
+                })}            
+            />
+            <TextInput placeholder="Quantidade do Produto" 
+                keyboardType='numeric'
+                value={produto.quantidade?.toString()}
+                onChangeText = {quantidade => setProduto({
+                    ...produto,
+                    quantidade: quantidade
+                })}
+             />
+            <TextInput placeholder="Unidade do Produto" 
+                keyboardType='numeric'
+                value={produto.valor?.toString()}
+                onChangeText = {valor => setProduto({
+                    ...produto,
+                    valor: valor
+                })}     
+                />
+            <TextInput placeholder="Marca do Produto" 
+                value={produto.unidade}
+                onChangeText = {unidade => setProduto({
+                    ...produto,
+                    unidade: unidade
+                })}     
+            />
+            <Text>Data de Validade</Text>
+            <DateField 
+                labelDate = 'Dia'
+                labelMonth = 'Mês'
+                labelYear = 'Ano'
+                value={produto.dataValidade}
+                onSubmit = {dataValidade => setProduto({
+                    ...produto,
+                    dataValidade: dataValidade
+                })}     
+            />
+            <Text>Data de Fabricação</Text>
+            <DateField 
+                labelDate = 'Dia'
+                labelMonth = 'Mês'
+                labelYear = 'Ano'
+                value={produto.dataFabricacao}
+                onSubmit = {dataFabricacao => setProduto({
+                    ...produto,
+                    dataFabricacao: dataFabricacao
+                })}     
+            />
         </View>
     );
 }
